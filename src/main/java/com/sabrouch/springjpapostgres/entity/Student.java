0c7c94d75046d79cf4d5effa241c13a23648fa2d
sabri baazaoui <baazaouisabri@gmail.com>
2021-01-23 21:59:49 +0100

    add student entity

---
+++ src/main/java/com/sabrouch/springjpapostgres/entity/Student.java
@@ -0,0 +1,32 @@
+package com.sabrouch.springjpapostgres.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * Created by sabrouch.
+ * Date: 1/23/2021
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@SequenceGenerator(name = "seq", sequenceName = "seq",allocationSize = 1)
+@Table( uniqueConstraints = {@UniqueConstraint(name = "email", columnNames ="email")})
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    @Column(nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private Integer age;
+}

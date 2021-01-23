2b1eff9aaca65acaef5a28b8276689a4de788d33
sabri baazaoui <baazaouisabri@gmail.com>
2021-01-23 21:44:27 +0100

    database configure

---
+++ src/main/java/com/sabrouch/springjpapostgres/SpringjpapostgresApplication.java
@@ -0,0 +1,14 @@
+package com.sabrouch.springjpapostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpapostgresApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringjpapostgresApplication.class, args);
+    }
+
+}

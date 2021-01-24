ec81132308fae815939c13acf76b395b8b3dbb4a
sabri baazaoui <baazaouisabri@gmail.com>
2021-01-24 18:29:36 +0100

    Faker java add

--- src/main/java/com/sabrouch/springjpapostgres/SpringjpapostgresApplication.java
+++ src/main/java/com/sabrouch/springjpapostgres/SpringjpapostgresApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,7 @@ public class SpringjpapostgresApplication {
     CommandLineRunner commandLineRunner(StudentRepository studentRepository){
         return args -> {
           generateListOfStudent(studentRepository);
+          studentRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
 
         };
     }
